cb53d583617a8a86f859c39f7be2f7c0e56fc220
giunchoe <z961223@gmail.com>
2024-01-13 16:04:59 +0900

    init directories

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return '''
-    <h1>이건 h1 제목</h1>
-    <p>이건 본문 </p>
-    <a href="https://flask.palletsprojects.com">Flask 홈페이지 바로가기</a>
-    '''
+@app.route('/test')
+def test():
+    return render_template("test.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
