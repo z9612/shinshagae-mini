4944f838c6e76ca8b4b9fbcec720c28923f80d24
giunchoe <z961223@gmail.com>
2024-01-15 17:41:33 +0900

    일정 추가 기능 제작중(1)

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import pymysql
 import db_config 
-from flask import Flask, render_template, jsonify
+from flask import *
 from flask_cors import CORS
 
 app = Flask(__name__)
 
 CORS(app)
 
+db_config.db_connection.get_db()
+event_dao = db_config.EventDao()
+
 @app.route('/')
 def home():
     return render_template('calendar_events.html')
+
+
+@app.route('/insert_event', methods=['POST'])
+def insert_event():
+    #시작 날짜/종료 일자 검증 로직 필요
+
+    #실제 db에 insert 하기
+    event_name = request.form['event_name']
+    memo = request.form['memo']
+    from_date = request.form['from_date']
+    end_date = request.form['end_date']
+    url = request.form['url']
+    priority = request.form['priority']
+
+    print(f"------------결과값 {event_name}, {memo}, {from_date} {end_date} {url} {priority} ------------")
+    event_dao.insert_event(event_name, memo, from_date,end_date, url, priority)
+
+    return redirect(url_for("home"))
+
 
 @app.route('/calendar-events')
 def calendar_events():
@@ -18,7 +40,9 @@ def calendar_events():
     try:
         conn = db_config.db_connection.get_db()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT id, title, url, class, UNIX_TIMESTAMP(start_date)*1000 as start, UNIX_TIMESTAMP(end_date)*1000 as end FROM event")
+
+        ## 자기자신의 일정만 보이게하기 where문에 추가 필요
+        cursor.execute("SELECT id, title, url, class, UNIX_TIMESTAMP(start_date)*1000 as start, UNIX_TIMESTAMP(end_date)*1000 as end FROM event") 
         rows = cursor.fetchall()
         resp = jsonify({'success' : 1, 'result' : rows})
         resp.status_code = 200
--- db_config.py
+++ db_config.py
@@ -12,3 +12,57 @@ class db_connection :
         )
             
         return project_db
+    
+class EventDao:
+    def __init__(self):
+            pass
+    def select_all(self):
+        cursor = db_connection.get_db().cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT * FROM event;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        db_connection.get_db().close()
+        return result
+        
+    def insert_event(self, title, memo, start_date, end_date, url, priority):
+        cursor = db_connection.get_db().cursor(pymysql.cursors.DictCursor)
+
+        print("priority ::: " , priority)
+        match priority :
+            case '높음' :
+                priority = 'event-important'
+            case '중간' :
+                priority = 'event-error'
+            case '낮음' :
+                priority = ''
+
+        sql = "insert into event(title, memo, start_date, end_date, url, class) values(%s, %s, %s, %s, %s, %s)"
+
+        result = cursor.execute(sql,(title, memo, start_date,end_date,url, priority))
+
+        print(f"insert_num :: {result}")
+
+        db_connection.get_db().close()
+        return result
+    
+    # def update_book(self, bookid, bookname, publisher, price):
+    #     cursor = db_connection.get_db().cursor(pymysql.cursors.DictCursor)
+    #     sql = "update book set bookname=%s, publisher=%s, price=%s where bookid=%s"
+    #     result = cursor.execute(sql,(bookname, publisher, price, bookid))
+
+    #     print(f"update_num :: {result}")
+
+    #     db_connection.get_db().close()
+    #     return result
+        
+    # def delete_book(self, bookid):
+    #     cursor = db_connection.get_db().cursor(pymysql.cursors.DictCursor)
+    #     sql = "delete from book where bookid=%s"
+
+    #     result = cursor.execute(sql,(bookid))
+
+    #     print(f"delete_num :: {result}")
+
+    #     db_connection.get_db().close()
+    #     return result
